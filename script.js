76ea05af1abb34ea06627a85ba371d566fe8c1ec
Borja Ares <76764937+BorjaAres@users.noreply.github.com>
2023-07-02 00:38:30 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 // Discount Calculation
 
+// Discount Calculation
+
 // Prompt the user to enter the product price
-let price = prompt("Enter the product price");
+let price = parseFloat(prompt("Enter the product price"));
 
 // Prompt the user to enter the discount percentage
-let userDiscount = prompt("Enter the discount percentage");
+let userDiscount = parseFloat(prompt("Enter the discount percentage"));
 
 const extraDiscount = 0.15; // Extra discount percentage
 
-// Remove non-digit characters from the user-entered discount percentage
-let discountNumber = userDiscount.replace(/\D/g, '');
 // Convert the discount percentage to a decimal value
-let decimalDiscount = discountNumber / 100;
+let decimalDiscount = userDiscount / 100;
+
 // Calculate the discount amount in money
 let discountAmount = price * decimalDiscount;
 
 // Calculate the total price after applying the discount
 let total = price - discountAmount;
 
+// Apply extra discount if the total price is greater than 100
 if (total > 100) {
-  // Apply extra discount if the total price is greater than 100
-  let extraDiscountAmount = total * extraDiscount;
+    let extraDiscountAmount = total * extraDiscount;
   let finalPrice = total - extraDiscountAmount;
   document.getElementById('myDiv1').innerHTML = "We have applied an additional 15% discount, so the final price of your purchase is " + finalPrice;
 } else {
